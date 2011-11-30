b75c6667aa477cf39379491dc2e02b6c64870ba6
bkonkle <brandon@konkle.us>
2011-11-30 09:58:37 -0600

    Resolve #7 by copying the DATABASE_POOL setting instead of assigning it directly

--- balancer/routers.py
+++ balancer/routers.py
@@ -11,14 +11,14 @@ class BasePoolRouter(object):
     A base class for routers that use a pool of databases defined by the
     DATABASE_POOL setting.
     """
-    
+
     def __init__(self):
         from django.conf import settings
         if isinstance(settings.DATABASE_POOL, dict):
             self.pool = settings.DATABASE_POOL.keys()
         else:
-            self.pool = settings.DATABASE_POOL
-    
+            self.pool = list(settings.DATABASE_POOL)
+
     def allow_relation(self, obj1, obj2, **hints):
         """Allow any relation between two objects in the pool"""
         if obj1._state.db in self.pool and obj2._state.db in self.pool:
@@ -73,21 +73,21 @@ class RoundRobinRouter(BasePoolRouter):
     A router that cycles over a pool of databases in order, evenly distributing
     the load.
     """
-    
+
     def __init__(self):
         super(RoundRobinRouter, self).__init__()
-        
+
         # Shuffle the pool so the first database isn't slammed during startup.
         random.shuffle(self.pool)
-        
+
         self.pool_cycle = itertools.cycle(self.pool)
-    
+
     def db_for_read(self, model, **hints):
         return self.get_next_db()
 
     def db_for_write(self, model, **hints):
         return self.get_next_db()
-    
+
     def get_next_db(self):
         return self.pool_cycle.next()
 
